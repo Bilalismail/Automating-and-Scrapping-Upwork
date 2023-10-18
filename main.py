5201a7ed1da55e23ee0f91d4d915b3d2faa3cc25
Bilal Ismail <bilal39@live.co.uk>
2023-10-18 09:46:45 +0100

    level 3 update

--- main.py
+++ main.py
@@ -17,9 +17,10 @@ def main():
     login_instance.userNameLogin()
     login_instance.passwordEntry()
     login_instance.secretEntry()
+    login_instance.downloadPDF()
     scrape_instance = Scrape(login_instance.getDriver(), SECRET, PASSWORD)
     data = Data(**scrape_instance.scrapeInfo()).dict()
-    login_instance.downloadPDF()
+    
     scrape_instance.dataFromPDF()
     time.sleep(5)
     with open('data.json', 'w') as json_file:
